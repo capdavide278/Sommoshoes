70d35bdd4186f134e9f88240a41d9f810e9ce01a
capdavide278 <franc.davi00@gmail.com>
2025-01-27 22:25:39 +0100

    animazione terminata

--- script/script.js
+++ script/script.js
@@ -3,5 +3,12 @@ const menuAttivo = document.querySelector('.menu-attivo')
 const navMenu = document.querySelector('.nav-menu')
 
 menuAttivo.addEventListener('click', () =>{
-    navMenu.classList.toggle('active')
+    if(navMenu.classList.contains('active')){
+        navMenu.classList.remove('active')
+        navMenu.classList.add('off')
+    }else{
+        navMenu.classList.remove('off')
+        navMenu.classList.add('active')
+    }
 })
+
